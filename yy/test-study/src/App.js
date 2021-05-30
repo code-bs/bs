02c3cafeff0aa804e1448492427f224d57276c7a
yuda1124 <yuda1124@naver.com>
2021-05-31 01:58:25 +0900

    feat: first test code

--- yy/test-study/src/App.js
+++ yy/test-study/src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="app">app</div>;
 }
 
 export default App;
--- yy/test-study/src/App.test.js
+++ yy/test-study/src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+describe('App', () => {
+  test('App 렌더링', () => {
+    render(<App />);
+    screen.getByText('app');
+  });
 });
